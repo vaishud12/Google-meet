23c5613931f9530ffe06730e2dcd762020ccf023
vaishud12 <vaishuu2397@gmail.com>
2024-03-05 23:27:41 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/GoogleMeet_clone/',
+  base:'/Google-meet/',
   plugins: [react()],
 })
